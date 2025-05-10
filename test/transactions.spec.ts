b64ff29429013b6940c75939be8c5621803f7bc2
Anderson Almeida <andersonpa10pa@gmail.com>
2025-05-10 15:30:32 -0300

    Teste para o summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -86,4 +86,36 @@ describe('Rotas de transações', () => {
       }),
     )
   })
+
+  it('listar o resumo', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies: string[] = createTransactionResponse.get(
+      'Set-Cookie',
+    ) as string[]
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
+  })
 })
